d2312f61b774e2798a6730def16d3be482a68983
Raghav Kukkar <kukkarraghav02@gmail.com>
2026-07-16 13:53:15 +0530

    Fixed server.js

--- src/server.js
+++ src/server.js
@@ -7,10 +7,3 @@ connectToDB()
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
 })
-
-
-
-
-// CtVB20PHywj52fss
-
-// mongodb+srv://kukkarraghav02_db_user:CtVB20PHywj52fss@cluster0.ppuonwt.mongodb.net/?appName=Cluster0
